55f5020831f49471e1530fcbe37a144652ed5fe1
simov <simeonvelichkov@gmail.com>
2016-07-22 15:30:49 +0300

    Clear browser's cache before running the tests

--- test/index.js
+++ test/index.js
@@ -3,15 +3,14 @@ var electron = require('electron')
 var app = electron.app
 var BrowserWindow = electron.BrowserWindow
 
-var win
-function createWindow () {
-  win = new BrowserWindow({})
-  win.maximize()
-  win.loadURL('file://' + __dirname + '/index.html')
-  win.webContents.openDevTools()
-  win.on('closed', () => {
-    win = null
-  })
-}
+var win = null
 
-app.on('ready', createWindow)
+app.on('ready', () => {
+  win = new BrowserWindow({})
+  win.webContents.session.clearCache(() => {
+    win.maximize()
+    win.loadURL('file://' + __dirname + '/index.html')
+    win.webContents.openDevTools()
+    win.on('closed', () => (win = null))
+  })
+})
